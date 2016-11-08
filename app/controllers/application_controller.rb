93863464cb7587bb60cb2449e990b874ea9686cf
Tran Van Tuan B <tran.van.tuanb@framgia.com>
2016-11-08 14:59:59 +0700

    Add edit/update user profile

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,4 +5,21 @@ class ApplicationController < ActionController::Base
   def render_404
     render file: "#{Rails.root}/public/404.html", layout: false, status: 404
   end
+
+  def verify_user
+    unless logged_in?
+      store_location
+      flash[:danger] = t ".please_log_in"
+      redirect_to login_url
+    end
+  end
+
+  def correct_user
+    load_user
+    redirect_to root_url unless is_user? @user
+  end
+
+  def verify_admin
+    redirect_to root_url unless current_user.admin?
+  end
 end
