3633b5cfabb9978ed514dd22f3860a7cdfe23d94
Tran Van Tuan B <tran.van.tuanb@framgia.com>
2016-11-08 08:24:12 +0700

    Add user login logout

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,6 +8,7 @@ class User < ApplicationRecord
   has_many :orders, class_name: Order.name, foreign_key: :user_id,
     dependent: :destroy
 
+  attr_accessor :remember_token
   validates :name , presence: true, length: {maximum: Settings.name_size_max}
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
   validates :email, presence: true, length: {maximum: Settings.email_length},
@@ -16,4 +17,36 @@ class User < ApplicationRecord
   has_secure_password
   validates :password, presence: true, length: {minimum: Settings.password_min},
     allow_nil: true
+
+  class << self
+    def digest string
+      cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST
+        : BCrypt::Engine.cost
+      BCrypt::Password.create string, cost: cost
+    end
+
+    def new_token
+      SecureRandom.urlsafe_base64
+    end
+  end
+
+  def remember
+    self.remember_token = User.new_token
+    update_attributes remember_digest: User.digest(remember_token)
+  end
+
+  def authenticated? attribute, token
+    digest = send "#{attribute}_digest"
+    return false if digest.nil?
+    BCrypt::Password.new(digest).is_password? token
+  end
+
+  def forget
+    update_attributes remember_digest: nil
+  end
+
+  private
+  def downcase_email
+    self.email = email.downcase
+  end
 end
